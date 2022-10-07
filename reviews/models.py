af4d56c4a9412adda147eaebe4c34940ad0834cc
jejoonlee <jejoonlee1996@gmail.com>
2022-10-07 14:01:31 +0900

    제준 second commit

--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Review(models.Model):
   title = models.CharField(max_length=100)
   content = models.TextField()
   movie_name = models.CharField(max_length=100)
-  grade = models.IntegerField()
+  grade = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
